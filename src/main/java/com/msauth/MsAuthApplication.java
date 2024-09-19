afef2497a02be6d3afe879afebf8eb955cb3c451
nihadamirov <nihadamirov98@gmail.com>
2024-09-19 22:08:19 +0400

    config eureka discovery client

--- src/main/java/com/msauth/MsAuthApplication.java
+++ src/main/java/com/msauth/MsAuthApplication.java
@@ -2,10 +2,12 @@ package com.msauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class MsAuthApplication {
 
     public static void main(String[] args) {
